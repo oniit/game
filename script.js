79e277db205e0bffa44ebbf1b0bcbaef7068d26f
oniit <117742545+oniit@users.noreply.github.com>
2022-11-18 06:26:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function gundu(give, guess){
+    const pgive = parseFloat(give.value)
+    const pguess = parseFloat(guess.value)
+    const cgive = Math.floor(Math.random() * 11)
+    const cguess = cgive + Math.floor(Math.random() * 11)
+    const count = pgive + cgive
+    if (pguess == count) {
+      alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} (Kamu jawab ${pguess} WOWWW)\n\nJAWABAN SEMPURNA Tebakanmu sangat akurat! \nCenayang kah?`)
+    } else if (Math.abs(count-pguess) < Math.abs(count-cguess)) {
+      alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} (Kamu jawab ${pguess}!!!)\n\nTebakan selisihmu lebih sedikit dari Robot\nHOREEEE Selamat, kamu menang!`)
+    } else if (Math.abs(count-pguess) > Math.abs(count-cguess)) {
+      alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} -> Kamu jawab ${pguess} :((\n\nSelisih tebakan Robot lebih sedikit, \nOH TIDAK!!! Kamu kalah...`)
+    } else {
+      alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} (aw, sama seperti jawabanmu)\n\nHasilnya SERI deh!`)
+    }
+  }
+  
