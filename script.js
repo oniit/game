32e070841de62181b1f168dad1c1e3c1808a188d
oniit <117742545+oniit@users.noreply.github.com>
2022-11-18 19:58:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
-function gundu(give, guess){
-  const pgive = parseFloat(give.value)
-  const pguess = parseFloat(guess.value)
-  const cgive = Math.floor(Math.random() * 11)
-  const cguess = cgive + Math.floor(Math.random() * 11)
-  const count = pgive + cgive
-  if (pguess == count) {
-    alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} (Kamu jawab ${pguess} WOWWW)\n\nJAWABAN SEMPURNA Tebakanmu sangat akurat! \nCenayang kah?`)
-  } else if (Math.abs(count-pguess) < Math.abs(count-cguess)) {
-    alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} (Kamu jawab ${pguess}!!!)\n\nTebakan selisihmu lebih sedikit dari Robot\nHOREEEE Selamat, kamu menang!`)
-  } else if (Math.abs(count-pguess) > Math.abs(count-cguess) && count != cguess) {
-    alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} -> Kamu jawab ${pguess} :((\n\nSelisih tebakan Robot lebih sedikit, \nOH TIDAK!!! Kamu kalah...`)
-  } else if (Math.abs(count-pguess) > Math.abs(count-cguess) && count == cguess) {
-    alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} -> Kamu jawab ${pguess} :((\n\nTebakan Robot sangat sempurna, \nMohon maaf Kamu kalah telak!`)
-  } else if (cguess == pguess) {
-    alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} (aw, sama seperti jawabanmu)\n\nHasilnya SERI deh!`)
+function lucky(){
+  
+}
+
+function gundu(){
+  const botGive = Math.floor(Math.random() * 11)
+  const botGuess = Math.floor(Math.random() * 11) + botGive
+  const playerGive = document.getElementById("give").value
+  const playerGuess = document.getElementById("guess").value
+  const count = botGive + parseFloat(playerGive)
+  const hasil = `<h4><span class="big">HASIL<br></span></h4>
+  Kelereng yang <b>Kamu</b> pertaruhkan: ${playerGive}<br>
+  Kelereng yang <b>Robot</b> pertaruhkan: ${botGive}<br>
+  &emsp;&emsp;&emsp;--------------------------------------------------- + <br>
+  <b><span class="big">Total akumulasi: ${count}</span></b><br><br>
+  Yang ditebak Robot: <b>${botGuess}</b>
+  `
+  
+  function getWinner () {
+    if (playerGuess == count) {
+    return (`${hasil} (Kamu jawab <b>${playerGuess}</b> WOWWW)<br><br>
+    <h4><span class="big">JAWABAN SEMPURNA Tebakan sangat akurat!<br>
+    Cenayang kah?</span></h4>`)
+  } else if (Math.abs(count-playerGuess) < Math.abs(count-botGuess)) {
+    return (`${hasil} (Kamu jawab <b>${playerGuess}</b>!!!)<br><br>
+    <h4><span class="big">Tebakanmu lebih sedikit dari Robot,<br>
+    HOREEEE Selamat, kamu menang!</span></h4>`)
+  } else if (Math.abs(count-playerGuess) > Math.abs(count-botGuess) && count != botGuess) {
+    return (`${hasil} -> Kamu jawab <b>${playerGuess}</b> :((<br><br>
+    <h4><span class="big">Selisih tebakan Robot lebih sedikit,<br>
+    OH TIDAK!!! Kamu kalah...</span></h4>`)
+  } else if (Math.abs(count-playerGuess) > Math.abs(count-botGuess) && count == botGess) {
+    return (`${hasil} -> Kamu jawab <b>${playerGuess}</b> :((<br><br>
+    <h4><span class="big">Tebakan Robot sangat sempurna,<br>
+    Mohon maaf Kamu kalah telak!</span></h4>`)
+  } else if (botGuess == playerGess) {
+    return (`${hasil} (aw, sama seperti jawabanmu)<br><br>
+    <h4><span class="big">Hasilnya SERI deh!</span></h4>`)
   } else {
-    alert(`\n-------HASIL-------\nKelereng yang Kamu pertaruhkan: ${pgive}\nKelereng yang Robot pertaruhkan: ${cgive}\n\nTotal akumulasi: ${count}\nYang ditebak Robot: ${cguess} (aw, selisihnya sama)\n\nHasilnya SERI deh!`)
+    return (`${hasil} (aw, selisihnya sama)<br><br>
+    <h4><span class="big">Hasilnya SERI deh!</span></h4>`)
   }
 }
+  const winner = getWinner()
+  const info = document.querySelector('.info')
+  info.innerHTML = winner
+}
+
+function clearInput(){
+  var getValue1 = document.getElementById("give");
+  var getValue2 = document.getElementById("guess");
+  if (getValue1.value !="") {
+    getValue1.value = "";
+  }
+  if (getValue2.value !="") {
+    getValue2.value = "";
+  }
+ }
